4bc08eb069a9d755e29bb93c9eeea045bf42d99e
Anthony N <anthony-ngu@users.noreply.github.com>
2014-10-08 14:50:28 -0700

    Fixed the argument comparison checks and the exception logging. Added millis, micros, delayMicroseconds, shiftIn, and shiftOut. Also fixed the project file to use the most recent version of NodeJS (0.10.32) and uses the location c:\node-v0.10.32

--- nodewiring/nodewiring.cpp
+++ nodewiring/nodewiring.cpp
@@ -19,7 +19,7 @@ Handle<Value> IOInit(const Arguments& args) {
 Handle<Value> DigitalWrite(const Arguments& args) {
 	HandleScope scope;
 
-	if (args.Length() < 2) {
+	if (args.Length() != 2) {
 		return ThrowException(
 			Exception::TypeError(v8::String::New("Must pass 2 arguments, pin and state"))
 			);
@@ -35,9 +35,9 @@ Handle<Value> DigitalWrite(const Arguments& args) {
 Handle<Value> DigitalRead(const Arguments& args) {
 	HandleScope scope;
 
-	if (args.Length() < 1) {
+	if (args.Length() != 1) {
 		return ThrowException(
-			Exception::TypeError(v8::String::New("Must pass 1 argument to read."))
+			Exception::TypeError(v8::String::New("Must pass 1 argument, the pin to read."))
 			);
 	}
 
@@ -51,7 +51,7 @@ Handle<Value> DigitalRead(const Arguments& args) {
 
 Handle<Value> AnalogWrite(const Arguments& args) {
 	HandleScope scope;
-	if (args.Length() < 2) {
+	if (args.Length() != 2) {
 		return ThrowException(
 			Exception::TypeError(v8::String::New("Must pass 2 arguments, pin and value."))
 			);
@@ -66,9 +66,9 @@ Handle<Value> AnalogWrite(const Arguments& args) {
 
 Handle<Value> AnalogRead(const Arguments& args) {
 	HandleScope scope;
-	if (args.Length() < 1) {
+	if (args.Length() != 1) {
 		return ThrowException(
-			Exception::TypeError(v8::String::New("Must pass 1 argument to read."))
+			Exception::TypeError(v8::String::New("Must pass 1 argument, the pin to read."))
 			);
 	}
 
@@ -78,31 +78,90 @@ Handle<Value> AnalogRead(const Arguments& args) {
 	return scope.Close(Integer::New(value));
 }
 
+Handle<Value> Millis(const Arguments& args) {
+    HandleScope scope;
+    unsigned long value = millis();
+    return scope.Close(Uint32::New(value));;
+}
+
+Handle<Value> Micros(const Arguments& args) {
+    HandleScope scope;
+    unsigned long value = micros();
+    return scope.Close(Uint32::New(value));;
+}
 
 Handle<Value> Delay(const Arguments& args) {
 	HandleScope scope;
-	if (args.Length() < 1) {
+	if (args.Length() != 1) {
 		return ThrowException(
 			Exception::TypeError(v8::String::New("Must pass a value in ms."))
 			);
 	}
 	Local<Integer> val = args[0]->ToInteger();
 	unsigned long ms = static_cast<unsigned long>(val->Value());
+
 	delay(ms);
+
 	return Undefined();
 }
 
 
+Handle<Value> DelayMicroseconds(const Arguments& args) {
+    HandleScope scope;
+    if (args.Length() != 1) {
+        return ThrowException(
+            Exception::TypeError(v8::String::New("Must pass a value in microseconds."))
+            );
+    }
+    Local<Uint32> val = args[0]->ToUint32();
+    unsigned long ms = static_cast<unsigned long>(val->Value());
+    delayMicroseconds(ms);
+    return Undefined();
+}
+
 Handle<Value> PinMode(const Arguments& args) {
-	if (args.Length() < 2) {
+	if (args.Length() != 2) {
 		return ThrowException(
 			Exception::TypeError(v8::String::New("Must pass 2 arguments, pin and mode."))
 			);
 	}
 	Local<Integer> pin = args[0]->ToInteger();
 	Local<Integer> mode = args[1]->ToInteger();
+
 	pinMode(static_cast<int>(pin->Value()), static_cast<int>(mode->Value()));
+
 	return Undefined();
+}
+
+Handle<Value> ShiftIn(const Arguments& args) {
+    if (args.Length() != 3) {
+        return ThrowException(
+            Exception::TypeError(v8::String::New("Must pass 3 arguments, data_pin, clock_pin, and bit_order."))
+            );
+    }
+    Local<Integer> data_pin = args[0]->ToInteger();
+    Local<Integer> clock_pin = args[1]->ToInteger();
+    Local<Integer> bit_order = args[2]->ToInteger();
+
+    shiftIn(static_cast<uint8_t>(data_pin->Value()), static_cast<uint8_t>(clock_pin->Value()), static_cast<uint8_t>(bit_order->Value()));
+
+    return Undefined();
+}
+
+Handle<Value> ShiftOut(const Arguments& args) {
+    if (args.Length() != 4) {
+        return ThrowException(
+            Exception::TypeError(v8::String::New("Must pass 4 arguments, data_pin, clock_pin, bit_order, and byte."))
+            );
+    }
+    Local<Integer> data_pin = args[0]->ToInteger();
+    Local<Integer> clock_pin = args[1]->ToInteger();
+    Local<Integer> bit_order = args[2]->ToInteger();
+    Local<Integer> byte = args[3]->ToInteger();
+
+    shiftOut(static_cast<uint8_t>(data_pin->Value()), static_cast<uint8_t>(clock_pin->Value()), static_cast<uint8_t>(bit_order->Value()), static_cast<uint8_t>(byte->Value()));
+
+    return Undefined();
 }
 
 /*
@@ -129,9 +188,24 @@ extern "C" void NODE_EXTERN init(Handle<Object> target)
 
 	target->Set(v8::String::NewSymbol("analogRead"),
 		FunctionTemplate::New(AnalogRead)->GetFunction());
-    
-	target->Set(v8::String::NewSymbol("delay"),
-		FunctionTemplate::New(Delay)->GetFunction());
+
+    target->Set(v8::String::NewSymbol("delay"),
+        FunctionTemplate::New(Delay)->GetFunction());
+
+    target->Set(v8::String::NewSymbol("delayMicroseconds"),
+        FunctionTemplate::New(DelayMicroseconds)->GetFunction());
+
+    target->Set(v8::String::NewSymbol("millis"),
+        FunctionTemplate::New(Millis)->GetFunction());
+
+    target->Set(v8::String::NewSymbol("micros"),
+        FunctionTemplate::New(Micros)->GetFunction());
+
+    target->Set(v8::String::NewSymbol("shiftIn"),
+        FunctionTemplate::New(ShiftIn)->GetFunction());
+
+    target->Set(v8::String::NewSymbol("shiftOut"),
+        FunctionTemplate::New(ShiftOut)->GetFunction());
 }
 
 NODE_MODULE(nodewiring, init)
